d5dfb601d8b5dc1da7f27ef3b671502e22854cfd
Gustavo Maciel <gustamaciel2012@hotmail.com>
2019-12-11 21:19:22 -0300

    Update source.lua
    Adding "local" to variables

--- source.lua
+++ source.lua
@@ -1,10 +1,10 @@
 --1st program
 
-fs = "Affine function // MADE BY: gustavottc" --[[text variable]]
-se = "This program will resolve questions of affine function alone, \nTo use the program, follow the instructions that will be follow: \n -----\nWhat is the value of 'a'? = "--[[variable string]]
-tr = "What is the value of 'x'? = "--[[text variable]]
-fr = "What is the value of 'b'? = "--[[text variable]]
-ss = "The result is: "--[[text variable]]
+local fs = "Affine function // MADE BY: gustavottc" --[[text variable]]
+local se = "This program will resolve questions of affine function alone, \nTo use the program, follow the instructions that will be follow: \n -----\nWhat is the value of 'a'? = "--[[variable string]]
+local tr = "What is the value of 'x'? = "--[[text variable]]
+local fr = "What is the value of 'b'? = "--[[text variable]]
+local ss = "The result is: "--[[text variable]]
 
 io.write (fs .. "\n ---------- \n" .. se) --[[show text]]
 a = io.read() --[[data capture]]
